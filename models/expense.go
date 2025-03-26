bb268b66f8ab03ed533f361882d571ec02472145
Linderval-Moura <lindervalmatias@gmail.com>
2025-03-25 23:48:36 -0300

    Redefinindo estrutura da despesa

--- models/expense.go
+++ models/expense.go
@@ -2,20 +2,9 @@ package models
 
 import "time"
 
-// Expense define a estrutura de uma despesa
 type Expense struct {
-	ID          int
-	Description string
-	Amount      float64
-	Date        time.Time
-}
-
-// NewExpense cria uma nova despesa
-func NewExpense(id int, description string, amount float64) Expense {
-	return Expense{
-		ID:          id,
-		Description: description,
-		Amount:      amount,
-		Date:        time.Now(),
-	}
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Amount      float64   `json:"amount"`
+	Date        time.Time `json:"date"`
 }
