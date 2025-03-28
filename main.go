ad48d5f6c5eed5b09db773dac41327891dfb8ee3
Linderval Matias <72979747+Linderval-Moura@users.noreply.github.com>
2025-03-28 13:42:02 -0300

    Usando 8080 como padrão se nenhuma porta for especificada

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	port := os.Getenv("PORT") // Busca a porta das variáveis de ambiente
 	if port == "" {
 		fmt.Println("Environment PORT não declarada!")
-		port := ("8080")
+		port = ("8080")
 		fmt.Println("Servidor rodando na porta 8080...") // Usa 8080 como padrão se nenhuma porta for especificada 		
 	}
 
